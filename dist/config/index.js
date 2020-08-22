4883abb24bd6937d0e01590cef0388bd2d7e0356
JMullings <j_mullings@hotmail.co.uk>
2020-08-22 15:55:02 +0800

    dist added

---
+++ dist/config/index.js
@@ -0,0 +1,48 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.secureLayer = exports.secureConfig = exports.corsConfig = exports.envConfig = exports.ZIP_URL = exports.ZIP_FILE = exports.SEC_PATH = void 0;
+exports.SEC_PATH = 'secure_layer';
+exports.ZIP_FILE = 'secure-layer.zip';
+exports.ZIP_URL = 'https://serverless-secure-files.s3-ap-southeast-1.amazonaws.com/secure-layer.zip';
+exports.envConfig = {
+    STAGE: '${self:provider.stage}'
+};
+exports.corsConfig = {
+    corsValue: {
+        origin: '*',
+        headers: [
+            'Content-Type',
+            'X-Amz-Date',
+            'Authorization',
+            'X-Api-Key',
+            'X-Amz-Security-Token',
+            'X-Amz-User-Agent',
+            'Access-Control-Allow-Headers',
+            'Access-Control-Allow-Origin',
+            'x-app-token',
+            'x-user-token',
+            'Cache-Control'
+        ],
+        allowCredentials: true
+    }
+};
+exports.secureConfig = {
+    generateToken: {
+        handler: 'secure_layer/handler.generateToken',
+        events: [
+            {
+                http: {
+                    path: 'get_token',
+                    method: 'post',
+                    private: true
+                }
+            }
+        ]
+    },
+    secureAuthorizer: {
+        handler: 'secure_layer/handler.secureAuthorizer'
+    }
+};
+exports.secureLayer = {
+    SecureDependenciesNodeModule: { path: 'secure_layer', description: 'secure dependencies' }
+};
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+require("./before-hooks");
+var ServerlessSecure_1 = require("./ServerlessSecure");
+module.exports = ServerlessSecure_1.ServerlessSecure;
