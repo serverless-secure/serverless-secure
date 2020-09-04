9055e79052084dccbe50546ee9f5df65105a7c80
JMullings <j_mullings@hotmail.co.uk>
2020-09-04 17:51:30 +0800

    General updates

---
+++ dist/secure/index.d.ts
@@ -0,0 +1,52 @@
+import { TSConfigUpdate } from './ts-update';
+import Serverless from 'serverless';
+import * as _ from 'lodash';
+export declare class ServerlessSecure {
+    private yawn;
+    hooks: object;
+    private isYaml;
+    commands: object;
+    private content;
+    private serverless;
+    private sourceFile;
+    private functionList;
+    options: {
+        path: string;
+        p: string;
+    };
+    private baseTS;
+    private baseYAML;
+    constructor(serverless?: Serverless, options?: any);
+    apply(): Promise<void>;
+    beforeFile(): void;
+    beforePath(): Promise<void>;
+    afterPath(): Promise<void>;
+    static parseHttpPath(_path: string): string;
+    pathExists(_path: string): Promise<boolean>;
+    updateEnv(content: {
+        [x: string]: any;
+    }): any;
+    updateCustom(content: {
+        [x: string]: any;
+    }): any;
+    updateLayers(content: {
+        [x: string]: any;
+    }): any;
+    updateApiKeys(content: {
+        provider: any;
+    }): unknown[];
+    updateFunctions(content: {
+        [x: string]: any;
+    }): Promise<any>;
+    contentUpdate(_content: any): any;
+    parseTS(_content: any): Promise<any>;
+    parseYAML(_content: any): Promise<any>;
+    ignoreErrors(sourceFile: TSConfigUpdate): string;
+    writeTS(sourceFile: TSConfigUpdate): Promise<void>;
+    writeYAML(content: Serverless): Promise<void>;
+    downloadSecureLayer(): Promise<void>;
+    unZipPackage(extractPath: string, _path: string): Promise<void>;
+    deleteFile(extractPath: string): Promise<void>;
+    notification(message: string, type: string): void;
+    parseFile(arr: _.List<unknown> | null | undefined, top: number | undefined, bot: number | undefined): any;
+}
