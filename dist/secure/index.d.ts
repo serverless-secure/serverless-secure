9850592c52a82581740c1d2d20bdf33b610c3452
serverless-secure <admin@serverless-secure.com>
2021-01-26 08:13:36 +0000

    Triggered Build: 25

---
+++ dist/secure/index.d.ts
@@ -0,0 +1,54 @@
+import { TSConfigUpdate } from './ts-update';
+import Serverless from 'serverless';
+export declare class ServerlessSecure {
+    private yawn;
+    hooks: object;
+    private isYaml;
+    private ApiKey;
+    commands: object;
+    private content;
+    private serverless;
+    private sourceFile;
+    private functionList;
+    options: {
+        path: string;
+        p: string;
+    };
+    private baseTS;
+    private baseYAML;
+    private baseLayer;
+    constructor(serverless?: Serverless, options?: any);
+    apply(): Promise<void>;
+    beforeFile(): void;
+    afterPath(): Promise<void>;
+    beforePath(): Promise<void>;
+    static parseHttpPath(_path: string): string;
+    pathExists(_path: string): Promise<boolean>;
+    updateEnv(content: {
+        [x: string]: any;
+    }): any;
+    updateCustom(content: {
+        [x: string]: any;
+    }): any;
+    updateLayers(content: {
+        [x: string]: any;
+    }): any;
+    updateApiKeys(content: {
+        provider: any;
+    }): unknown[];
+    setOptions(ele: any): Promise<void>;
+    updateFunctions(content: {
+        [x: string]: any;
+    }): Promise<any>;
+    contentUpdate(_content: any): any;
+    parseTS(_content: any): Promise<any>;
+    parseYAML(_content: any): Promise<any>;
+    ignoreErrors(sourceFile: TSConfigUpdate): string;
+    writeTS(sourceFile: TSConfigUpdate): Promise<void>;
+    writeYAML(content: Serverless): Promise<void>;
+    mkdirRecursively(folderpath: any): boolean;
+    downloadSecureLayer(): Promise<void>;
+    unZipPackage(zip: any, data: any): Promise<void>;
+    deleteFolder(extractPath: string): Promise<void>;
+    notification(message: string, type: string): void;
+}
